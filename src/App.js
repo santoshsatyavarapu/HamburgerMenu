d52af098f27ce1ad314a61f6bcc5117baf0e26fe
root <santoshsatyavarapu123@gamil.com>
2023-05-14 05:49:15 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Home from './components/Home'
+import About from './components/About'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/bad-path" component={NotFound} />
+    <Redirect to="bad-path" />
+  </Switch>
+)
+
+export default App
